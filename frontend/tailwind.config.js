022174474111c1df07e16973b579ed8ebaa7339e
CorvuZ1 <superman876@mail.ru>
2023-10-17 22:05:58 +0300

    add ui framework

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   important: true,
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}"],
-  plugins: []
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
 };
