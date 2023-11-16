676c32adb2450eedd65a2877f6e3c1eda14cff8c
CorvuZ1 <superman876@mail.ru>
2023-11-16 22:01:31 +0200

    Replace ui framework

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
 
+const isDev = process.env.NODE_ENV === "development";
+const modulesPath = isDev ? "../node_modules" : "node_modules";
 
-module.exports = {
-  important: true,
+module.exports = withMT({
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    `${modulesPath}/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}`,
+    `${modulesPath}/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}`
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-};
+  plugins: [],
+});
